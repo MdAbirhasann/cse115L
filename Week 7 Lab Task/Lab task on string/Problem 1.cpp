5f219da4b89f9565b095786fef53ff340733f301
Md. Abir hasan <68628409+MdAbirhasann@users.noreply.github.com>
2020-09-09 23:45:27 +0600

    Lab task added

---
+++ Week 7 Lab Task/Lab task on string/Problem 1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[10];
+    int i;
+    for(i=0;i<10;i++)
+    {
+        scanf("%c",&str[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%c",str[i]);
+    }
+    return 0;
+}
